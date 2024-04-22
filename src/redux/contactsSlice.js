ff871e8c199e8b6f8156e635e94be417e61fb602
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-04-22 13:17:12 +0300

    1

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const INITIAL_STATE = {
+  contacts: { items: [] },
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: INITIAL_STATE,
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.items.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.contacts.items = state.contacts.items.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+export default contactsSlice.reducer;
